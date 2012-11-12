ec409b0acdf0d107085f26e74248e48bbce85a14
Jón Atli <thasnowbear@yahoo.dk>
2012-11-12 14:59:25 +0000

    Added Project

---
+++ src/RandomClass.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Notandi
+ * Date: 12.11.2012
+ * Time: 14:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class RandomClass {
+    public RandomClass() {
+    }
+}
