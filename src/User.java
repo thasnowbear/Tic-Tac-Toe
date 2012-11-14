96b0c10a1b354e2db6ebbec1480b6afd9bfbeeda
Eric Nielsen <nielsene54@gmail.com>
2012-11-14 14:09:44 +0000

    Updated user class

--- src/User.java
+++ src/User.java
@@ -6,4 +6,27 @@
  * To change this template use File | Settings | File Templates.
  */
 public class User {
+    private String name;
+    private int won;
+
+    public User(String name, int won) {
+        this.name = name;
+        this.won = won;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWon() {
+        return won;
+    }
+
+    public void setWon(int won) {
+        this.won = won;
+    }
 }
